0e49967dbbf14a187b21659872503e06f6eb12ec
Nathaniel Jason <nthnieljson@gmail.com>
2020-10-31 19:40:03 +0700

    answer panel component

--- src/components/AnswerPanel.js
+++ src/components/AnswerPanel.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { textQuestion } from "../constant";
+import { useQuestion } from "../context/questions";
 
 const AnswerPanel = () => {
   //   const questionId = textQuestion.question.map((question) => question.id);
   const size = 20;
   const questionId = [...Array(size).keys()].map((i) => i + 1);
+  // const { answers } = useQuestion();
   const answers = {
     1: "b",
     3: "c",
