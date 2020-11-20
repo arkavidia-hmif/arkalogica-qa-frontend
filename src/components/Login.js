4e0960809ccf112a93a203432435551fff368f96
arsa dg <arsaadg891@gmail.com>
2020-11-20 14:05:48 +0700

    login styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -46,36 +46,65 @@ const Login = () => {
 
   return (
     <div>
-      <div>
-        <h2>Login</h2>
-        <form onSubmit={postLogin}>
-          <div>
-            <label>Email </label>
-            <input
-              name="email"
-              type="text"
-              placeholder="Email"
-              value={values.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              name="password"
-              type="password"
-              placeholder="Password"
-              value={values.password}
-              onChange={handleChange}
-            />
-          </div>
-          <button>Sign In</button>
-        </form>
+      <div className="container">
+        <h2>Arkalogica Login</h2>
+        <div className="row">
+          <form onSubmit={postLogin} className="col-lg-6">
+            <div>
+              <label>Email </label>
+              <input
+                name="email"
+                type="text"
+                placeholder="Email"
+                value={values.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+              />
+            </div>
+            <button className="btn-primary mt-3 btn-lg">Login</button>
+          </form>
+        </div>
         {isError && <p>The username or password provided were incorrect!</p>}
       </div>
-      <Countdown />
-      <AnswerPanel />
+      <style jsx>{`
+        h2 {
+          text-align: center;
+        }
+        form {
+          margin-top: 5rem;
+        }
+        label {
+          font-style: normal;
+          font-weight: bold;
+          font-size: 1.4rem;
+          line-height: 1.2rem;
+          display: block;
+          color: #000000;
+          margin-top: 0.8rem;
+        }
+        input {
+          width: 100%;
+          border: none;
+          padding: 0.5rem 0 0.5rem 0;
+          border-bottom: 0.15rem solid black;
+          box-sizing: border-box;
+          background: none;
+          margin: 0.5rem 0 1rem 0;
+        }
+        input:focus {
+          outline: none;
+        }
+      `}</style>
     </div>
   );
 };
