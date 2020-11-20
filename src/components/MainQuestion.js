002bb77effba391d4bd2571c45bb45771c0d9840
arsa dg <arsaadg891@gmail.com>
2020-11-20 14:21:24 +0700

    add sidebar mainquestion

--- src/components/MainQuestion.js
+++ src/components/MainQuestion.js
@@ -2,6 +2,8 @@ import React from "react";
 import { FE_ARKALOGICA_PARAM } from "../constant";
 import { useQuestion } from "../context/questions";
 import { Link } from "react-router-dom";
+import Countdown from "./Countdown";
+import AnswerPanel from "./AnswerPanel";
 
 export default () => {
   const { isSessionStarted, session } = useQuestion();
@@ -9,15 +11,39 @@ export default () => {
   const time = String(new Date(session?.endTime));
   return isSessionStarted ? (
     <div className="container">
-      <h1>{session?.title}</h1>
-      <p className="font-weight-bold">End time: {time}</p>
-      <p>{session?.description}</p>
-      <Link
-        to={FE_ARKALOGICA_PARAM + "/" + String(session?.question[0]?.id)}
-        className="btn btn-primary"
-      >
-        Challenge
-      </Link>
+      <div className="row">
+        <div className="col-lg-3 col-md-4 col-6 top-left-container">
+          <Countdown />
+          <AnswerPanel />
+        </div>
+        <div className="col-lg-9">
+          <h1>{session?.title}</h1>
+          <p className="font-weight-bold">End time: {time}</p>
+          <p>{session?.description}</p>
+          <Link
+            to={FE_ARKALOGICA_PARAM + "/" + String(session?.question[0]?.id)}
+            className="btn btn-primary"
+          >
+            Challenge
+          </Link>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .top-left-container {
+            padding: 1em;
+            background: #5cdb95;
+            border-radius: 1em;
+            max-height: 50em;
+          }
+          @media screen and (max-width: 992px) {
+            .top-left-container {
+              align-items: center;
+              margin-bottom: 2em;
+            }
+          }
+        `}
+      </style>
     </div>
   ) : (
     <>
